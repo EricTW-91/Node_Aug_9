ca99a6f97af829e495fa4142b2dea6b7545346dd
EricTW-91 <amperic05@gmail.com>
2021-08-10 01:08:14 -0700

    API updated

---
+++ API.js
@@ -0,0 +1,47 @@
+const os = require('os')
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('hello world')
+})
+
+app.get('/api/arch', (req, res) => {
+    const arch = {
+        architecture: os.arch()
+    }
+    res.send(arch)
+})
+app.get('/api/cpus', (req, res) => {
+    const cpus = {
+        cpus: os.cpus()
+    }
+    res.send(cpus)
+})
+app.get('/api/ram', (req, res) => {
+    const ram = {
+        ram: `${os.totalmem() / 1024 / 1024 / 1024} GB`
+    }
+    res.send(ram)
+})
+app.get('/api/diskspace', (req, res) => {
+    res.send('Can not find it!')
+})
+app.get('/api/hostname', (req, res) => {
+    const hostname = {
+        hostname: os.hostname()
+    }
+    res.send(hostname)
+})
+app.get('/api/ipaddress', (req, res) => {
+    const ip = {
+        address: os.networkInterfaces()
+    }
+    res.send(ip)
+})
+
+
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Listening on port ${port}`))
---
+++ CLI.js
@@ -0,0 +1,10 @@
+const process = require('process')
+
+console.log(process.argv)
+
+// switch (process.argv[2]) {
+//     case 'arch':
+
+//         break
+    
+// }
