51cc19c0293550a234d857988d8f728e106c156c
EricTW-91 <amperic05@gmail.com>
2021-08-13 13:33:01 -0700

    htmlServer updated

--- d1/htmlServer.js
+++ d1/htmlServer.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     
-	fs.readFile('index.html', (err, data) => {
+	fs.readFile('../index.html', (err, data) => {
 		//checking for errors
 		if (err) 
 			throw err;
--- d4/test.js
+++ d4/test.js
@@ -13,3 +13,21 @@ console.timeEnd('Count')
 if (count > 100) {
     console.error('The count is greater than 100!')
 }
+
+// path
+// basename([string])
+const path = require('path')
+console.log(path.basename('/foo/baz/index.js'))
+// return index.js
+
+
+// crypto
+const crypto = require('crypto')
+console.log(crypto.randomInt(10))
+
+const num = Math.random()
+console.log(num)
+console.log(num*20)
+console.log(Math.floor(num*20))
+
+
